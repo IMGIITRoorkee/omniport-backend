f4cc7aa1a1af10243a051b5e5a91230c7a69f326
Dhruv Bhanushali <dhruv_b@live.com>
2018-03-29 18:25:26 +0530

    Register all models in the Django admin

---
+++ kernel/admin/__init__.py
@@ -0,0 +1,18 @@
+import swapper
+
+from kernel.admin.site import omnipotence
+from kernel.models import User
+
+# Load all swappable models
+Person = swapper.load_model('kernel', 'Person')
+BiologicalInformation = swapper.load_model('kernel', 'BiologicalInformation')
+FinancialInformation = swapper.load_model('kernel', 'FinancialInformation')
+PoliticalInformation = swapper.load_model('kernel', 'PoliticalInformation')
+
+# Register all models
+# If any are being overridden, they will show up separately in the Django admin
+omnipotence.register(User)
+omnipotence.register(Person)
+omnipotence.register(BiologicalInformation)
+omnipotence.register(FinancialInformation)
+omnipotence.register(PoliticalInformation)
