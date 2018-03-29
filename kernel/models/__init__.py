0d234f0a01342ef2ab1bf81c8911feda85af2d13
Dhruv Bhanushali <dhruv_b@live.com>
2018-03-29 15:47:23 +0530

    Since models are read from models.py, import them into __init__.py for Django to recognize them

--- kernel/models/__init__.py
+++ kernel/models/__init__.py
@@ -1,1 +1,17 @@
 from kernel.models.auth import User
+
+from kernel.models.person import (
+    AbstractPerson,
+
+    Person,
+)
+
+from kernel.models.personal_information import (
+    AbstractBiologicalInformation,
+    AbstractPoliticalInformation,
+    AbstractFinancialInformation,
+
+    BiologicalInformation,
+    FinancialInformation,
+    PoliticalInformation,
+)
---
+++ kernel/models/personal_information/__init__.py
@@ -0,0 +1,15 @@
+from kernel.models.personal_information.biological_information import (
+    AbstractBiologicalInformation,
+
+    BiologicalInformation,
+)
+from kernel.models.personal_information.financial_information import (
+    AbstractFinancialInformation,
+
+    FinancialInformation,
+)
+from kernel.models.personal_information.political_information import (
+    AbstractPoliticalInformation,
+
+    PoliticalInformation,
+)
