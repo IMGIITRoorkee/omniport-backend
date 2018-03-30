f3644f2214c53147aa227aa24acc0cfa72610d7a
Dhruv Bhanushali <dhruv_b@live.com>
2018-03-31 02:42:48 +0530

    Create a function that gets users based on various fields from related models

---
+++ kernel/managers/get_user.py
@@ -0,0 +1,61 @@
+import swapper
+from django.db.models import Q
+
+from kernel.models import User, ContactInformation
+
+
+def get_user(username):
+    """
+    Get the user corresponding to the given username
+    :param username: the username provided to identify the user
+    :return: the user, if a user with the given username is found
+    :raise: User.DoesNotExist, if no user with the given username is found
+    """
+
+    """
+    This method is different from User.objects.get(username=username) because
+    it looks at a spectrum of other fields in related models to identify the 
+    user. These fields are
+
+    User
+    - username
+
+    ContactInformation
+    - primary_phone_number
+    - secondary_phone_number
+    - email_address
+    - institute_webmail_address
+    """
+
+    Person = swapper.load_model('kernel', 'Person')
+
+    try:
+        user = User.objects.get(username=username)
+        return user
+    except User.DoesNotExist:
+        pass
+
+    try:
+        q_primary_phone_number = Q(primary_phone_number=username)
+        q_secondary_phone_number = Q(secondary_phone_number=username)
+        q_email_address = Q(email_address=username)
+        q_institute_webmail_address = Q(institute_webmail_address=username)
+        q = (
+                q_primary_phone_number
+                | q_secondary_phone_number
+                | q_email_address
+                | q_institute_webmail_address
+        )
+        contact_information = ContactInformation.objects.get(q)
+        entity = contact_information.entity
+        if type(entity) is Person:
+            user = entity.user
+            if user is not None:
+                return user
+    except (
+            ContactInformation.DoesNotExist,
+            User.DoesNotExist
+    ):
+        pass
+
+    raise User.DoesNotExist
