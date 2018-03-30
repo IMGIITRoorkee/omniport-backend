9678299aeff3a4c0b60284c4f9e4cf72a235f3a6
Dhruv Bhanushali <dhruv_b@live.com>
2018-03-31 02:44:02 +0530

    Register ContactInformation and LocationInformation in the admin site

--- kernel/admin/__init__.py
+++ kernel/admin/__init__.py
@@ -1,7 +1,11 @@
 import swapper
 
 from kernel.admin.site import omnipotence
-from kernel.models import User
+from kernel.models import (
+    User,
+    ContactInformation,
+    LocationInformation,
+)
 
 # Load all swappable models
 Person = swapper.load_model('kernel', 'Person')
@@ -12,7 +16,11 @@ PoliticalInformation = swapper.load_model('kernel', 'PoliticalInformation')
 # Register all models
 # If any are being overridden, they will show up separately in the Django admin
 omnipotence.register(User)
+
 omnipotence.register(Person)
 omnipotence.register(BiologicalInformation)
 omnipotence.register(FinancialInformation)
 omnipotence.register(PoliticalInformation)
+
+omnipotence.register(ContactInformation)
+omnipotence.register(LocationInformation)
