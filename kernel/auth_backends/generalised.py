cdcca323614585945a50a0709a5ffabdc5933c07
Dhruv Bhanushali <dhruv_b@live.com>
2018-03-31 02:43:27 +0530

    Write an auth backend based on the new get_user function allowing people to login with various fields as their username

---
+++ kernel/auth_backends/generalised.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.backends import ModelBackend
+
+from kernel.managers.get_user import get_user
+from kernel.models import User
+from kernel.utils.rights import has_alohomora_rights
+
+
+class GeneralisedAuthBackend(ModelBackend):
+    """
+    This auth backend authenticates users via a complex relation for username,
+    wherein the User instance is searched by the get_user function defined in
+    the kernel.utils file.
+
+    This auth backend also has Alohomora support baked right in, so that
+    debugging and helping users is easy. This backend allows
+    accessor-authenticated Alohomora, which is more secure than shared password
+    Alohomora which requires big changes if that password gets leaked. Also this
+    method restricts Alohomora access to users explicitly granted that right.
+    """
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None or password is None:
+            return None
+
+        if '_alohomora_' in username:
+            (account_holder, account_accessor) = username.split('_alohomora_')
+        else:
+            (account_holder, account_accessor) = (username, username)
+
+        try:
+            account_holder = get_user(username=account_holder)
+            account_accessor = get_user(username=account_accessor)
+            if (
+                    has_alohomora_rights(account_accessor)
+                    and account_accessor.check_password(password)
+            ):
+                return account_holder
+            else:
+                return None
+        except User.DoesNotExist:
+            return None
