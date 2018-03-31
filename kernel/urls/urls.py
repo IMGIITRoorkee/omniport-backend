562ed1eb97b44c62c557c47465a8ea0a0694d0b1
Dhruv Bhanushali <dhruv_b@live.com>
2018-04-01 02:28:20 +0530

    Add and register views pertaining to auth tasks like login, logout, change and reset password

--- kernel/urls/urls.py
+++ kernel/urls/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from kernel.views.auth import (
+    Login,
+    ChangePassword,
+    ResetPassword,
+    Logout,
+)
 from kernel.views.home import Home
 from kernel.views.who_am_i import WhoAmI
 
@@ -7,5 +13,11 @@ app_name = 'kernel'
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+
     path('who_am_i/', WhoAmI.as_view(), name='who_am_i'),
+
+    path('login/', Login.as_view(), name='login'),
+    path('change_password/', ChangePassword.as_view(), name='change_password'),
+    path('reset_password/', ResetPassword.as_view(), name='reset_password'),
+    path('logout/', Logout.as_view(), name='logout'),
 ]
---
+++ kernel/views/auth.py
@@ -0,0 +1,244 @@
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from kernel.errors.auth import (
+    INCORRECT_CREDENTIALS,
+    USER_DOES_NOT_EXIST,
+    RESET_ATTEMPTS_EXHAUSTED,
+)
+from kernel.errors.forms import INCOMPLETE_DATA
+from kernel.managers.get_user import get_user
+from kernel.models.auth import User
+from kernel.permissions.is_not_authenticated import IsNotAuthenticated
+
+
+class Login(APIView):
+    """
+    This view takes the username and password and if correct, logs the user in
+    via cookie-based session authentication
+
+    Works only when not authenticated
+    """
+
+    permission_classes = (IsNotAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        """
+        View to serve POST requests
+        :param request: the request that is to be responded to
+        :param args: arguments
+        :param kwargs: keyword arguments
+        :return: the response for request
+        """
+
+        errors = list()
+
+        username = request.data.get('username', None)
+        password = request.data.get('password', None)
+
+        if username is not None and password is not None:
+            user = authenticate(
+                request,
+                username=username,
+                password=password
+            )
+            if user is not None:
+                login(request, user)
+                response = {
+                    'data': {
+                        'type': 'message',
+                        'attributes': {
+                            'text': f'Successfully logged in as {user}',
+                        },
+                    },
+                }
+                return Response(response)
+            else:
+                errors.append(INCORRECT_CREDENTIALS)
+        else:
+            errors.append(INCOMPLETE_DATA)
+
+        response = {
+            'errors': errors,
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePassword(APIView):
+    """
+    This view takes the old password and the new password and if the old
+    password is correct, changes the password to the new one
+
+    Works only when authenticated
+    """
+
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        """
+        View to serve POST requests
+        :param request: the request that is to be responded to
+        :param args: arguments
+        :param kwargs: keyword arguments
+        :return: the response for request
+        """
+
+        errors = list()
+
+        old_password = request.data.get('old_password', None)
+        new_password = request.data.get('new_password', None)
+
+        if old_password is not None and new_password is not None:
+            user = request.user
+            if user.check_password(old_password):
+                user.set_password(new_password)
+                user.save()
+                response = {
+                    'data': {
+                        'type': 'message',
+                        'attributes': {
+                            'text': 'Successfully changed password',
+                        },
+                    },
+                }
+                return Response(response)
+            else:
+                errors.append(INCORRECT_CREDENTIALS)
+        else:
+            errors.append(INCOMPLETE_DATA)
+
+        response = {
+            'errors': errors,
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ResetPassword(APIView):
+    """
+    This view, when responding to a GET request, shows the secret question for
+    the user in question and, when responding to a POST request, takes the
+    username, the secret_answer and the new password to reset it
+
+    Works only when not authenticated
+    """
+
+    permission_classes = (IsNotAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        """
+        View to serve GET requests
+        :param request: the request that is to be responded to
+        :param args: arguments
+        :param kwargs: keyword arguments
+        :return: the response for request
+        """
+
+        errors = list()
+
+        username = request.query_params.get('username', None)
+
+        if username is not None:
+            try:
+                user = get_user(username)
+                secret_question = user.secret_question
+                response = {
+                    'data': {
+                        'type': 'secret_question',
+                        'attributes': {
+                            'text': secret_question,
+                        },
+                    },
+                }
+                return Response(response)
+            except User.DoesNotExist:
+                errors.append(USER_DOES_NOT_EXIST)
+        else:
+            errors.append(INCOMPLETE_DATA)
+
+        response = {
+            'errors': errors,
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, *args, **kwargs):
+        """
+        View to serve POST requests
+        :param request: the request that is to be responded to
+        :param args: arguments
+        :param kwargs: keyword arguments
+        :return: the response for request
+        """
+
+        errors = list()
+
+        username = request.data.get('username', None)
+        secret_answer = request.data.get('secret_answer', None)
+        new_password = request.data.get('new_password', None)
+
+        if (
+                username is not None
+                and secret_answer is not None
+                and new_password is not None
+        ):
+            try:
+                user = get_user(username)
+                if user.failed_reset_attempts < 3:
+                    if user.check_secret_answer(secret_answer):
+                        user.set_password(new_password)
+                        user.save()
+                        response = {
+                            'data': {
+                                'type': 'message',
+                                'attributes': {
+                                    'text': 'Successfully changed password',
+                                },
+                            },
+                        }
+                        return Response(response)
+                    else:
+                        errors.append(INCORRECT_CREDENTIALS)
+                else:
+                    errors.append(RESET_ATTEMPTS_EXHAUSTED)
+            except User.DoesNotExist:
+                errors.append(USER_DOES_NOT_EXIST)
+        else:
+            errors.append(INCOMPLETE_DATA)
+
+        response = {
+            'errors': errors,
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Logout(APIView):
+    """
+    This view deletes the cookie-based session authentication token from the
+    database, thereby logging out the user
+
+    Works only when authenticated
+    """
+
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        """
+        View to serve GET requests
+        :param request: the request that is to be responded to
+        :param args: arguments
+        :param kwargs: keyword arguments
+        :return: the response for request
+        """
+
+        logout(request)
+        response = {
+            'data': {
+                'type': 'message',
+                'attributes': {
+                    'text': 'Successfully logged out',
+                },
+            },
+        }
+        return Response(response)
