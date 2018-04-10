0dcfdf881f73f1c5c8972376360fa1ad40c6e595
Dhruv Bhanushali <dhruv_b@live.com>
2018-04-11 04:33:27 +0530

    Add a context preprocessor that loads branding logos and text in the context

---
+++ omniport/context/branding.py
@@ -0,0 +1,69 @@
+import os
+
+from django.conf import settings
+from django.contrib.staticfiles import finders
+
+
+def unique_logo(name, extension):
+    """
+    Check if a logo with the given name exists
+    :param name: the name of the logo to check for existence
+    :param extension: the extension of the logo to check for existence
+    :return: the relative path to the logo if found, None otherwise
+    """
+
+    logo_relative_path = os.path.join(
+        'omniport',
+        'logos',
+        f'{name}{extension}',
+    )
+    if finders.find(logo_relative_path) is not None:
+        return logo_relative_path
+    else:
+        return None
+
+
+def indexed_logo(name, extension, preferred_index):
+    """
+    Check if a logo with the given name, and preferably index, exists
+    :param name: the name of the logo to check for existence
+    :param extension: the extension of the logo to check for existence
+    :param preferred_index: the preferred index of the logo
+    :return: the relative path to the logo if found, None otherwise
+    """
+
+    logo_relative_path = os.path.join(
+        'omniport',
+        'logos',
+        f'{name}_{preferred_index}{extension}',
+    )
+    if finders.find(logo_relative_path) is not None:
+        return logo_relative_path
+    else:
+        return unique_logo(name, extension)
+
+
+def logos(_):
+    """
+    Add the logos of the institute, maintainer and project to the context
+    :param _: the request being served
+    :return: the data to add to the context
+    """
+
+    data = dict()
+    data['INSTITUTE_LOGO'] = unique_logo('institute', '.png')
+    data['MAINTAINERS_LOGO'] = unique_logo('maintainers', '.png')
+    data['PORTAL_LOGO'] = indexed_logo('portal', '.png', settings.SITE_ID)
+    return data
+
+
+def branding_text(_):
+    """
+    Add the brand text of the institute, maintainer and project to the context
+    :param _: the request being served
+    :return: the data to add to the context
+    """
+
+    data = dict()
+    data['INSTITUTE'] = settings.INSTITUTE
+    data['MAINTAINERS'] = settings.MAINTAINERS
--- omniport/settings/base.py
+++ omniport/settings/base.py
@@ -121,6 +121,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'omniport.context.branding.logos',
                 'omniport.context.site.site_information',
             ],
         },
