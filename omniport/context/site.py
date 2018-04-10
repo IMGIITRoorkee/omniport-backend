28a716a41d18bf31443d9128d595833c0535fe7f
Dhruv Bhanushali <dhruv_b@live.com>
2018-04-11 04:28:37 +0530

    Load the SITE_ID in the context as well for any template that needs it

--- omniport/context/site.py
+++ omniport/context/site.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 
 
-def site_information(request):
+def site_information(_):
     """
     Add the site information from settings to the context
-    :param request: the request being served
+    :param _: the request being served
     :return: the data to add to the context
     """
 
     data = dict()
+    data['SITE_ID'] = settings.SITE_ID
     data['SITE_NAME'] = settings.SITE_NAME
     data['SITE_VERBOSE_NAME'] = settings.SITE_VERBOSE_NAME
     return data
