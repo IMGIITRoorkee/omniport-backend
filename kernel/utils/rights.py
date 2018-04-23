9a41689259db9514cbec4edea20273adf166fa02
Dhruv Bhanushali <dhruv_b@live.com>
2018-04-23 16:25:54 +0530

    Allow the shell to override the rights functions from kernel

--- kernel/utils/rights.py
+++ kernel/utils/rights.py
@@ -1,3 +1,15 @@
+"""
+These functions are an essential part of the rights framework used by Omniport.
+They determine which users have access to the administrative features of the
+project, namely
+- omnipotence, the admin interface
+- alohomora, the impersonation ability
+- lockpicking, the no-questions-asked password reset ability
+
+These can, and most probably should, be overridden in shell.utils.rights
+"""
+
+
 def has_omnipotence_rights(user):
     """
     Check if the given user has enough privileges to access the admin interface
@@ -5,7 +17,11 @@ def has_omnipotence_rights(user):
     :return: True if the user has privileges, False otherwise
     """
 
-    return True  # TODO: Find a proper way to determine privileges
+    try:
+        from shell.utils.rights import has_omnipotence_rights as omnipotence
+        return omnipotence(user)
+    except ImportError:
+        return False
 
 
 def has_alohomora_rights(user):
@@ -15,7 +31,11 @@ def has_alohomora_rights(user):
     :return: True if the user has privileges, False otherwise
     """
 
-    return True  # TODO: Find a proper way to determine privileges
+    try:
+        from shell.utils.rights import has_alohomora_rights as alohomora
+        return alohomora(user)
+    except ImportError:
+        return False
 
 
 def has_lockpicking_rights(user):
@@ -25,4 +45,8 @@ def has_lockpicking_rights(user):
     :return: True if the user has privileges, False otherwise
     """
 
-    return True  # TODO: Find a proper way to determine privileges
+    try:
+        from shell.utils.rights import has_lockpicking_rights as lockpicking
+        return lockpicking(user)
+    except ImportError:
+        return False
---
+++ shell/utils/rights.py
@@ -0,0 +1,28 @@
+def has_omnipotence_rights(user):
+    """
+    Check if the given user has enough privileges to access the admin interface
+    :param user: the user whose privileges are being tested
+    :return: True if the user has privileges, False otherwise
+    """
+
+    return True  # TODO: Find a proper way to determine privileges
+
+
+def has_alohomora_rights(user):
+    """
+    Check if the given user has enough privileges to impersonate another user
+    :param user: the user whose privileges are being tested
+    :return: True if the user has privileges, False otherwise
+    """
+
+    return True  # TODO: Find a proper way to determine privileges
+
+
+def has_lockpicking_rights(user):
+    """
+    Check if the given user has enough privileges to reset anyone's password
+    :param user: the user whose privileges are being tested
+    :return: True if the user has privileges, False otherwise
+    """
+
+    return True  # TODO: Find a proper way to determine privileges
