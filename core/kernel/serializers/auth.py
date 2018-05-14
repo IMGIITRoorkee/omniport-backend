24a3be94058644a7104f6ae04ba4e17d84aed034
Dhruv Bhanushali <dhruv_b@live.com>
2018-05-14 16:37:55 +0530

    Use serializers and GenericAPIView instead of APIView in password-related views

---
+++ core/kernel/serializers/auth.py
@@ -0,0 +1,119 @@
+import random
+import string
+
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import PasswordField
+
+from kernel.managers.get_user import get_user
+from kernel.models import User
+
+
+class UserRetrievalSerializer(serializers.Serializer):
+    """
+    Stores an instance of User with the given username
+    """
+
+    user = None
+    username = serializers.CharField()
+
+    def validate_username(self, username):
+        """
+        Validates the username by seeing if any User object matches
+        :param username: the username whose existence is being checked
+        :return: the username after validation
+        :raise ValidationError: if no user has the given username
+        """
+
+        try:
+            user = get_user(username=username)
+            self.user = user
+        except User.DoesNotExist:
+            raise serializers.ValidationError('Non-existent username')
+
+        return username
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    """
+    Stores the old and the new password for a user intending to change it
+    """
+
+    old_password = PasswordField()
+    new_password = PasswordField()
+
+    def validate_old_password(self, old_password):
+        """
+        Validates the old password by checking if it authenticates the user
+        :param old_password: the old password for a user
+        :return: the old password if it authenticates the user
+        :raise ValidationError: if the old password is incorrect
+        """
+
+        user = self.context.get('request').user
+
+        if not user.check_password(old_password):
+            raise serializers.ValidationError('Incorrect old password')
+
+        return old_password
+
+
+class ResetPasswordSerializer(UserRetrievalSerializer):
+    """
+    Stores the secret answer and the new password for changing it
+    """
+
+    secret_answer = serializers.CharField()
+    new_password = PasswordField()
+
+    def validate_secret_answer(self, secret_answer):
+        """
+        Validate the secret answer by comparing it with the stored answer
+        :param secret_answer: the secret answer to check for correctness
+        :return: the secret answer if it is correct
+        :raise ValidationError: if the secret answer is wrong
+        """
+
+        if not self.user.check_secret_answer(secret_answer):
+            raise serializers.ValidationError('Incorrect credentials')
+
+        return secret_answer
+
+    def validate(self, data):
+        """
+        Check if the user in question has reset attempts left
+        :param data: the data to check for validity
+        :return: the data if the user has not exceeded failed attempts
+        :raise ValidationError: if the user has failed to reset password thrice
+        """
+
+        if self.user.failed_reset_attempts >= 3:
+            raise serializers.ValidationError('Reset attempts exceeded limit')
+
+        return data
+
+
+class LockpickingSerializer(UserRetrievalSerializer):
+    """
+    Stores the new password if it is being reset by a maintainer
+    """
+
+    new_password = PasswordField(required=False)
+
+    def validate(self, data):
+        """
+        Generate a random password in case one is not provided
+        :param data: the data to check for validity
+        :return: the data
+        """
+
+        new_password = data.get('new_password', None)
+        if not new_password:
+            allowed_chars = string.ascii_uppercase + string.digits
+            password_length = 8
+            new_password = ''.join(
+                random.SystemRandom().choice(allowed_chars)
+                for _ in range(password_length)
+            )
+            data['new_password'] = new_password
+
+        return data
--- core/kernel/views/auth.py
+++ core/kernel/views/auth.py
@@ -1,24 +1,18 @@
-import random
-import string
-
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
-from kernel.errors.auth import (
-    INCORRECT_CREDENTIALS,
-    USER_DOES_NOT_EXIST,
-    RESET_ATTEMPTS_EXHAUSTED,
-)
-from kernel.errors.forms import INCOMPLETE_DATA
-from kernel.managers.get_user import get_user
-from kernel.models.auth import User
 from kernel.permissions.has_lockpicking_rights import HasLockpickingRights
-from kernel.permissions.is_not_authenticated import IsNotAuthenticated
+from kernel.serializers.auth import (
+    UserRetrievalSerializer,
+    ChangePasswordSerializer,
+    ResetPasswordSerializer,
+    LockpickingSerializer,
+)
 
 
-class ChangePassword(APIView):
+class ChangePassword(GenericAPIView):
     """
     This view takes the old password and the new password and if the old
     password is correct, changes the password to the new one
@@ -27,6 +21,8 @@ class ChangePassword(APIView):
     """
 
     permission_classes = (IsAuthenticated,)
+
+    serializer_class = ChangePasswordSerializer
 
     def post(self, request, *args, **kwargs):
         """
@@ -37,47 +33,37 @@ class ChangePassword(APIView):
         :return: the response for request
         """
 
-        errors = list()
-
-        old_password = request.data.get('old_password', None)
-        new_password = request.data.get('new_password', None)
-
-        if old_password is not None and new_password is not None:
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            new_password = serializer.validated_data.get('new_password')
             user = request.user
-            if user.check_password(old_password):
-                user.set_password(new_password)
-                user.failed_reset_attempts = 0
-                user.save()
-                response = {
-                    'data': {
-                        'type': 'message',
-                        'attributes': {
-                            'text': 'Successfully changed password',
-                        },
+            user.set_password(new_password)
+            user.failed_reset_attempts = 0
+            user.save()
+            response = {
+                'data': {
+                    'type': 'message',
+                    'attributes': {
+                        'text': 'Successfully changed password',
                     },
-                }
-                return Response(response)
-            else:
-                errors.append(INCORRECT_CREDENTIALS)
+                },
+            }
+            return Response(response, status=status.HTTP_200_OK)
         else:
-            errors.append(INCOMPLETE_DATA)
-
-        response = {
-            'errors': errors,
-        }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            response = {
+                'errors': serializer.errors,
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ResetPassword(APIView):
+class ResetPassword(GenericAPIView):
     """
     This view, when responding to a GET request, shows the secret question for
     the user in question and, when responding to a POST request, takes the
     username, the secret_answer and the new password to reset it
-
-    Works only when not authenticated
     """
 
-    permission_classes = (IsNotAuthenticated,)
+    serializer_class = ResetPasswordSerializer
 
     def get(self, request, *args, **kwargs):
         """
@@ -88,32 +74,25 @@ class ResetPassword(APIView):
         :return: the response for request
         """
 
-        errors = list()
-
         username = request.query_params.get('username', None)
-
-        if username is not None:
-            try:
-                user = get_user(username)
-                secret_question = user.secret_question
-                response = {
-                    'data': {
-                        'type': 'secret_question',
-                        'attributes': {
-                            'text': secret_question,
-                        },
+        serializer = UserRetrievalSerializer(data={'username': username})
+        if serializer.is_valid():
+            user = serializer.user
+            secret_question = user.secret_question
+            response = {
+                'data': {
+                    'type': 'secret_question',
+                    'attributes': {
+                        'text': secret_question,
                     },
-                }
-                return Response(response)
-            except User.DoesNotExist:
-                errors.append(USER_DOES_NOT_EXIST)
+                },
+            }
+            return Response(response, status=status.HTTP_200_OK)
         else:
-            errors.append(INCOMPLETE_DATA)
-
-        response = {
-            'errors': errors,
-        }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            response = {
+                'errors': serializer.errors,
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request, *args, **kwargs):
         """
@@ -124,49 +103,30 @@ class ResetPassword(APIView):
         :return: the response for request
         """
 
-        errors = list()
-
-        username = request.data.get('username', None)
-        secret_answer = request.data.get('secret_answer', None)
-        new_password = request.data.get('new_password', None)
-
-        if (
-                username is not None
-                and secret_answer is not None
-                and new_password is not None
-        ):
-            try:
-                user = get_user(username)
-                if user.failed_reset_attempts < 3:
-                    if user.check_secret_answer(secret_answer):
-                        user.set_password(new_password)
-                        user.failed_reset_attempts = 0
-                        user.save()
-                        response = {
-                            'data': {
-                                'type': 'message',
-                                'attributes': {
-                                    'text': 'Successfully changed password',
-                                },
-                            },
-                        }
-                        return Response(response)
-                    else:
-                        errors.append(INCORRECT_CREDENTIALS)
-                else:
-                    errors.append(RESET_ATTEMPTS_EXHAUSTED)
-            except User.DoesNotExist:
-                errors.append(USER_DOES_NOT_EXIST)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.user
+            new_password = serializer.validated_data.get('new_password')
+            user.set_password(new_password)
+            user.failed_reset_attempts = 0
+            user.save()
+            response = {
+                'data': {
+                    'type': 'message',
+                    'attributes': {
+                        'text': 'Successfully reset password',
+                    },
+                },
+            }
+            return Response(response, status=status.HTTP_200_OK)
         else:
-            errors.append(INCOMPLETE_DATA)
-
-        response = {
-            'errors': errors,
-        }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            response = {
+                'errors': serializer.errors,
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
-class Lockpick(APIView):
+class Lockpick(GenericAPIView):
     """
     This view takes the username and optionally, a new password and if an
     authorised user is logged in, changes the password for the user with the
@@ -178,6 +138,8 @@ class Lockpick(APIView):
 
     permission_classes = (IsAuthenticated, HasLockpickingRights,)
 
+    serializer = LockpickingSerializer
+
     def post(self, request, *args, **kwargs):
         """
         View to serve POST requests
@@ -187,40 +149,25 @@ class Lockpick(APIView):
         :return: the response for request
         """
 
-        errors = list()
-
-        username = request.data.get('username', None)
-        new_password = request.data.get('new_password', None)
-
-        if username is not None:
-            try:
-                user = get_user(username)
-                if new_password is None:
-                    allowed_chars = string.ascii_uppercase + string.digits
-                    password_length = 8
-                    new_password = ''.join(
-                        random.SystemRandom().choice(allowed_chars)
-                        for _ in range(password_length)
-                    )
-                user.set_password(new_password)
-                user.failed_reset_attempts = 0
-                user.save()
-                response = {
-                    'data': {
-                        'type': 'message',
-                        'attributes': {
-                            'text': 'Successfully changed password',
-                            'password': new_password,
-                        },
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.user
+            new_password = serializer.validated_data.get('new_password')
+            user.set_password(new_password)
+            user.failed_reset_attempts = 0
+            user.save()
+            response = {
+                'data': {
+                    'type': 'message',
+                    'attributes': {
+                        'text': 'Successfully changed password',
+                        'password': new_password,
                     },
-                }
-                return Response(response)
-            except User.DoesNotExist:
-                errors.append(USER_DOES_NOT_EXIST)
+                },
+            }
+            return Response(response, status=status.HTTP_200_OK)
         else:
-            errors.append(INCOMPLETE_DATA)
-
-        response = {
-            'errors': errors,
-        }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            response = {
+                'errors': serializer.errors,
+            }
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
