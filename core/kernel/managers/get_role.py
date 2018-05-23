a20a8c0f9ce10a675dc3e9a7ece75124c7600eed
Dhruv Bhanushali <dhruv_b@live.com>
2018-05-23 06:28:46 +0530

    Write the missing documentation for two parameters

--- core/kernel/managers/get_role.py
+++ core/kernel/managers/get_role.py
@@ -7,8 +7,8 @@ def get_role(person, role_name, filter_active=True, silent=False):
     Get a role corresponding to a person
     :param person: an instance of the Person model whose roles are sought
     :param role_name: the name of the role class whose instance is required
-    :param filter_active:
-    :param silent:
+    :param filter_active: whether to filter out the roles other than active
+    :param silent: whether to fail silently or raise exceptions
     :return: the role, if the person fulfills it
     :raise: Role.DoesNotExist, if the given role is not fulfilled by the person
     :raise: ImproperlyConfigured, if the name of the role class is incorrect
