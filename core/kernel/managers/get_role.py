3286a35ad4fb0a704df7e2a3654fe94a5f14cf27
Dhruv Bhanushali <dhruv_b@live.com>
2018-05-23 06:25:40 +0530

    Add the ability to filter roles based on whether they are active and whether and the option to either fail silently or raise exceptions

--- core/kernel/managers/get_role.py
+++ core/kernel/managers/get_role.py
@@ -1,16 +1,33 @@
 import swapper
+from django.core.exceptions import ImproperlyConfigured
 
 
-def get_role(person, role_name):
+def get_role(person, role_name, filter_active=True, silent=False):
     """
     Get a role corresponding to a person
     :param person: an instance of the Person model whose roles are sought
     :param role_name: the name of the role class whose instance is required
+    :param filter_active:
+    :param silent:
     :return: the role, if the person fulfills it
     :raise: Role.DoesNotExist, if the given role is not fulfilled by the person
     :raise: ImproperlyConfigured, if the name of the role class is incorrect
     """
 
-    Role = swapper.load_model('kernel', role_name)
-    role = Role.objects.get(person=person)
-    return role
+    try:
+        Role = swapper.load_model('kernel', role_name)
+        try:
+            if filter_active:
+                query_set = Role.all_filter_active()
+            else:
+                query_set = Role.objects.all()
+            role = query_set.get(person=person)
+            return role
+        except Role.DoesNotExist:
+            if not silent:
+                raise
+    except ImproperlyConfigured:
+        if not silent:
+            raise
+
+    return None
