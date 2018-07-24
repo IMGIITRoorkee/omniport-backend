44f05fc983ff8be0f043624364ff878292d24031
Dhruv Bhanushali <dhruv_b@live.com>
2018-07-24 12:43:26 +0530

    Remove the api/ prefix from websocket URLs because they already have the ws/ prefix

--- omniport/utils/urls.py
+++ omniport/utils/urls.py
@@ -52,16 +52,12 @@ def get_ws_urlpatterns(app_group):
         if app.get('isAllowed'):
             config = app.get('config')
             base_url = config.get('baseUrls', {}).get('ws', None)
-            is_api = config.get('isApi', False)
             if base_url is not None:
                 module = importlib.import_module(f'{name}.ws_urls')
                 dictionary = module.__dict__
                 app_urlpatterns = dictionary['urlpatterns']
                 url_router = URLRouter(app_urlpatterns)
-                if is_api:
-                    path_string = f'api/{base_url}'
-                else:
-                    path_string = f'{base_url}'
+                path_string = f'{base_url}'
                 urlpatterns.append(
                     path(path_string, url_router)
                 )
