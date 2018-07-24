0c8ac5ba773b4093c879cf5456f7782f3993098f
Dhruv Bhanushali <dhruv_b@live.com>
2018-07-24 21:47:34 +0530

    Delete the app protocol tests to make way for a more comprehensive test suite

--- core/protocol_tests/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProtocolTestsConfig(AppConfig):
-    name = 'protocol_tests'
-    verbose_name = 'Protocol tests'
--- core/protocol_tests/ws_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from protocol_tests.consumers import PingPong
-
-urlpatterns = [
-    # WebSocket Ping Pong!
-    path('ping_pong/', PingPong, name='ping_pong'),
-]
