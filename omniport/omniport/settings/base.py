45cabaee087c46192893c163de327602b3caea37
Shaddy Garg <shaddygarg1@gmail.com>
2018-09-20 21:44:01 +0530

    Add pagination to the default REST framework settings

--- omniport/omniport/settings/base.py
+++ omniport/omniport/settings/base.py
@@ -201,6 +201,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
 }
 
 # Static files
