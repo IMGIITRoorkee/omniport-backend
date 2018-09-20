8725c0faa69790262ebd2a7ffd9993b1290bf63e
Shaddy Garg <shaddygarg1@gmail.com>
2018-09-20 21:45:09 +0530

    Add ProfileSerializer and AvatarSerializer in person serializers

--- omniport/core/kernel/serializers/person.py
+++ omniport/core/kernel/serializers/person.py
@@ -15,3 +15,39 @@ class PersonSerializer(ModelSerializer):
 
         model = swapper.load_model('kernel', 'Person')
         fields = '__all__'
+
+
+class ProfileSerializer(ModelSerializer):
+    """
+    Serializer for the entire profile of a person
+    """
+
+    class Meta:
+        """
+        Meta class for ProfileSerializer
+        """
+
+        model = swapper.load_model('kernel', 'Person')
+        exclude = (
+            'datetime_modified',
+            'removed',
+        )
+
+
+class AvatarSerializer(ModelSerializer):
+    """
+    Serializer for minimal information of a person
+    """
+
+    class Meta:
+        """
+        Meta class for AvatarSerializer
+        """
+
+        model = swapper.load_model('kernel', 'Person')
+        fields = (
+            'short_name',
+            'full_name',
+            'display_picture',
+        )
+
