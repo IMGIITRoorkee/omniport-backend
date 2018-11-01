2c83e354612dabf2811f57540662ad9ce28ccaee
Dhruv Bhanushali <dhruv_b@live.com>
2018-11-01 06:02:13 +0530

    Remove the option to configure CORS externally and opt for hardcoding settings for consistency

--- omniport/omniport/settings/third_party/cors.py
+++ omniport/omniport/settings/third_party/cors.py
@@ -2,8 +2,6 @@
 This settings file exposes settings for CORS headers
 """
 
-from omniport.settings.configuration.base import configuration as _conf
-
-CORS_ALLOW_CREDENTIALS = _conf.cors.allow_credentials
-CORS_ORIGIN_WHITELIST = _conf.cors.origin_whitelist
-CORS_ORIGIN_ALLOW_ALL = _conf.cors.origin_allow_all
+CORS_ALLOW_CREDENTIALS = False
+CORS_ORIGIN_WHITELIST = list()
+CORS_ORIGIN_ALLOW_ALL = False
