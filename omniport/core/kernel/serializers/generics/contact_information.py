da7af03804a12aa2372d778dab9e48f38d6a1ed9
Vivek Chand <V.rajput40@gmail.com>
2018-11-27 20:04:17 +0530

    Add contact information serializer

---
+++ omniport/core/kernel/serializers/generics/contact_information.py
@@ -0,0 +1,25 @@
+import swapper
+
+from kernel.serializers.root import ModelSerializer
+from kernel.serializers.institute.department import DepartmentSerializer
+
+class ContactInformationSerializer(ModelSerializer):
+    """
+    Serializer for Contact information
+    """
+
+    class Meta:
+        """
+        Meta class for Contact information
+        """
+
+        model = swapper.load_model('kernel', 'ContactInformation')
+        fields = (
+            'primary_phone_number',
+            'secondary_phone_number',
+            'email_address',
+            'institute_webmail_address',
+            'email_address_verified',
+            'video_conference_id',
+        )
+
