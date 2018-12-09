545ed9e3a89f4c437264bf493fc54dac2e90b9da
Dhruv Bhanushali <dhruv_b@live.com>
2018-12-10 00:38:25 +0530

    Add `django.contrib.postgres` to INSTALLED_APPS

--- omniport/omniport/settings/base/discovery.py
+++ omniport/omniport/settings/base/discovery.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     # PyPI packages
     'channels',  # HTTP/2.0 and sockets support
