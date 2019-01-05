a3a9c749d4350bbe4f5a199ed93321f09d7d3825
Dhruv Bhanushali <dhruv_b@live.com>
2019-01-05 19:54:39 +0530

    Move the view listing all maintainers to the yellow pages service

--- omniport/core/kernel/http_urls.py
+++ omniport/core/kernel/http_urls.py
@@ -1,20 +1,12 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
-from kernel.views.maintainers import MaintainerViewSet
 from kernel.views.rights import Rights
 from kernel.views.who_am_i import WhoAmI
 
 app_name = 'kernel'
 
-# DRF routers
-router = routers.SimpleRouter()
-router.register('maintainers', MaintainerViewSet, base_name='maintainer')
-
 # URL patterns
 urlpatterns = [
     path('who_am_i/', WhoAmI.as_view(), name='who_am_i'),
     path('rights/', Rights.as_view(), name='rights'),
-
-    path('', include(router.urls)),
 ]
