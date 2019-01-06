d7392dfe4a312f770e1561d262e25a9a4a886382
Dhruv Bhanushali <dhruv_b@live.com>
2019-01-06 18:50:48 +0530

    Fix a bug in DegreeSerializer caused by graduation being a tuple

--- omniport/core/kernel/serializers/institute/degree.py
+++ omniport/core/kernel/serializers/institute/degree.py
@@ -1,4 +1,5 @@
 import swapper
+from rest_framework import serializers
 
 from kernel.serializers.root import ModelSerializer
 
@@ -7,6 +8,19 @@ class DegreeSerializer(ModelSerializer):
     """
     Serializer for Branch objects
     """
+
+    graduation = serializers.SerializerMethodField(
+        read_only=True,
+    )
+
+    def get_graduation(self, instance):
+        """
+        Return the graduation level of the degree, which is the first element
+        of the graduation tuple
+        :return: the first element of the graduation tuple
+        """
+
+        return instance.graduation[1]
 
     class Meta:
         """
