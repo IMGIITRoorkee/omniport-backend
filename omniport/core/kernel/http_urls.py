ec9215db4ab622862cfb3e3773e77445a85b9518
Dhruv Bhanushali <dhruv_b@live.com>
2019-05-16 16:01:44 +0530

    Add an alias for the `who_am_i` URL at `whoami` along the lines of the Linux command

--- omniport/core/kernel/http_urls.py
+++ omniport/core/kernel/http_urls.py
@@ -7,6 +7,8 @@ app_name = 'kernel'
 
 # URL patterns
 urlpatterns = [
-    path('who_am_i/', WhoAmI.as_view(), name='who_am_i'),
+    path('who_am_i/', WhoAmI.as_view(), name='who_am_i'),  # Primary URL
+    path('whoami/', WhoAmI.as_view(), name='whoami'),  # Alternate URL
+
     path('rights/', Rights.as_view(), name='rights'),
 ]
