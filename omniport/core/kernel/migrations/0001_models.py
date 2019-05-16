8d89bd295a79693e9fedc5646985e1a2c63e4e08
Dhruv Bhanushali <dhruv_b@live.com>
2019-05-16 23:59:53 +0530

    Add validations, refactor imports and drop the generic models which have been moved

--- omniport/core/kernel/migrations/0001_models.py
+++ omniport/core/kernel/migrations/0001_models.py
@@ -4,7 +4,8 @@ import django_countries.fields
 import swapper
 from django.db import migrations, models
 
-import kernel.utils.upload_to
+import formula_one.utils.upload_to
+import formula_one.validators.aspect_ratio
 
 
 class Migration(migrations.Migration):
@@ -160,7 +161,7 @@ class Migration(migrations.Migration):
                 ('datetime_modified', models.DateTimeField(auto_now=True)),
                 ('short_name', models.CharField(blank=True, max_length=63)),
                 ('full_name', models.CharField(max_length=255)),
-                ('display_picture', models.ImageField(blank=True, max_length=255, null=True, upload_to=kernel.utils.upload_to.UploadTo('kernel', 'display_pictures'))),
+                ('display_picture', models.ImageField(blank=True, max_length=255, null=True, upload_to=formula_one.utils.upload_to.UploadTo('kernel', 'display_pictures'), validators=[formula_one.validators.aspect_ratio.AspectRatioValidator(1)])),
             ],
             options={
                 'verbose_name_plural': 'people',
@@ -195,7 +196,7 @@ class Migration(migrations.Migration):
                 ('datetime_modified', models.DateTimeField(auto_now=True)),
                 ('local_bank', models.CharField(blank=True, max_length=63)),
                 ('local_bank_account_number', models.CharField(blank=True, max_length=31)),
-                ('annual_income', models.DecimalField(blank=True, decimal_places=2, max_digits=31, null=True)),
+                ('annual_income', models.DecimalField(blank=True, decimal_places=2, max_digits=31, null=True, validators=[django.core.validators.MinValueValidator(0)])),
             ],
             options={
                 'verbose_name_plural': 'financial information',
@@ -229,74 +230,6 @@ class Migration(migrations.Migration):
             options={
                 'verbose_name_plural': 'residential information',
                 'swappable': swapper.swappable_setting('kernel', 'ResidentialInformation'),
-            },
-        ),
-
-        # Generics
-        migrations.CreateModel(
-            name='ContactInformation',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('datetime_created', models.DateTimeField(auto_now_add=True)),
-                ('datetime_modified', models.DateTimeField(auto_now=True)),
-                ('primary_phone_number', models.CharField(blank=True, max_length=15, null=True, unique=True)),
-                ('secondary_phone_number', models.CharField(blank=True, max_length=15, null=True, unique=True)),
-                ('email_address', models.EmailField(blank=True, max_length=254, null=True, unique=True)),
-                ('email_address_verified', models.BooleanField(default=False)),
-                ('institute_webmail_address', models.EmailField(blank=True, max_length=254, null=True, unique=True)),
-                ('entity_object_id', models.PositiveIntegerField()),
-                ('video_conference_id', models.CharField(blank=True, max_length=127, null=True, unique=True, verbose_name='video conference ID')),
-                ('entity_content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.ContentType')),
-            ],
-            options={
-                'verbose_name_plural': 'contact information',
-            },
-        ),
-        migrations.CreateModel(
-            name='LocationInformation',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('datetime_created', models.DateTimeField(auto_now_add=True)),
-                ('datetime_modified', models.DateTimeField(auto_now=True)),
-                ('address', models.TextField()),
-                ('city', models.CharField(max_length=127)),
-                ('state', models.CharField(max_length=127)),
-                ('country', django_countries.fields.CountryField(max_length=2)),
-                ('postal_code', models.IntegerField(blank=True, null=True, validators=[django.core.validators.RegexValidator('[0-9]{3,9}')])),
-                ('latitude', models.DecimalField(blank=True, decimal_places=8, max_digits=12, null=True)),
-                ('longitude', models.DecimalField(blank=True, decimal_places=8, max_digits=12, null=True)),
-                ('entity_object_id', models.PositiveIntegerField()),
-                ('entity_content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.ContentType')),
-            ],
-            options={
-                'verbose_name_plural': 'location information',
-            },
-        ),
-        migrations.CreateModel(
-            name='SocialLink',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('datetime_created', models.DateTimeField(auto_now_add=True)),
-                ('datetime_modified', models.DateTimeField(auto_now=True)),
-                ('site', models.CharField(choices=[('beh', 'Behance'), ('blo', 'Blogger'), ('dri', 'Dribbble'), ('fac', 'Facebook'), ('fli', 'Flickr'), ('git', 'GitHub'), ('goo', 'Google'), ('lin', 'LinkedIn'), ('med', 'Medium'), ('pin', 'Pinterest'), ('red', 'Reddit'), ('sky', 'Skype'), ('sna', 'Snapchat'), ('tum', 'Tumblr'), ('twi', 'Twitter'), ('you', 'YouTube'), ('oth', 'Other website')], max_length=7)),
-                ('url', models.URLField(max_length=255, verbose_name='URL')),
-            ],
-            options={
-                'abstract': False,
-            },
-        ),
-        migrations.CreateModel(
-            name='SocialInformation',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('datetime_created', models.DateTimeField(auto_now_add=True)),
-                ('datetime_modified', models.DateTimeField(auto_now=True)),
-                ('entity_object_id', models.PositiveIntegerField()),
-                ('entity_content_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='contenttypes.ContentType')),
-                ('links', models.ManyToManyField(blank=True, to='kernel.SocialLink')),
-            ],
-            options={
-                'verbose_name_plural': 'social information',
             },
         ),
     ]
