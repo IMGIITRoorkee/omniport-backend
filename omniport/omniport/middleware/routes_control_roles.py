66c444658ce2520da8e3ac91bc8042572544bb4d
rish9101 <rranjan@cs.iitr.ac.in>
2019-10-16 12:32:43 +0530

    Add middleware for guest role routes control

---
+++ omniport/omniport/middleware/routes_control_roles.py
@@ -0,0 +1,44 @@
+from base_auth.managers.get_user import get_user
+
+from django.conf import settings
+from django.http import Http404
+
+class RoutesControlRoles:
+    """
+    Handles the routing of apps for the guest role and restricts
+    the guest user to the allowed urls of an app only
+    """
+    def __init__(self, get_response):
+        """
+        __init__function exactly as the Django documentation says for
+        a custom middleware
+        """
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        source_user = request.user
+        if source_user == get_user('Guest User'):
+            """
+            Perform actual processing on the request before it goes to the view
+            and on response returned by the view
+            :param request: the request being processed
+            :return: the processed response
+            """
+            
+            DISCOVERY = settings.DISCOVERY
+
+            all_apps = DISCOVERY.apps
+
+            for app, app_configuration in all_apps:
+                base_url = app_configuration.base_urls.http.strip('/')
+
+                excluded_paths = app_configuration.excluded_paths.excluded_paths
+
+                for excluded_path in excluded_paths:
+                    if re.match(f'^/{base_url}/{excluded_path}/', request.path):
+                        raise Http404
+                
+        response = self.get_response(request)
+
+        return response
