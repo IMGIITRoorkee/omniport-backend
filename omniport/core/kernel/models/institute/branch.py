b8da800688354b29951611327c14341cb56f279b
Ayush Jain <ayushjain20n@gmail.com>
2019-12-11 23:17:45 +0530

    Add a limit choice generic key from branch to centre and department

--- omniport/core/kernel/models/institute/branch.py
+++ omniport/core/kernel/models/institute/branch.py
@@ -1,5 +1,7 @@
 import swapper
 from django.db import models
+from django.contrib.contenttypes import fields as contenttypes_fields
+from django.contrib.contenttypes import models as contenttypes_models
 
 from formula_one.models.base import Model
 
@@ -10,9 +12,23 @@ class AbstractBranch(Model):
     in the college
     """
 
-    department = models.ForeignKey(
-        to=swapper.get_model_name('kernel', 'Department'),
+    # Relationship with the department or center entity
+    _limits = models.Q(
+        app_label=swapper.get_model_name('kernel', 'Department').split('.')[0],
+        model='department',
+    ) | models.Q(
+        app_label=swapper.get_model_name('kernel', 'Centre').split('.')[0],
+        model='centre',
+    )
+    entity_content_type = models.ForeignKey(
+        to=contenttypes_models.ContentType,
         on_delete=models.CASCADE,
+        limit_choices_to=_limits,
+    )
+    entity_object_id = models.BigIntegerField()
+    content_object = contenttypes_fields.GenericForeignKey(
+        ct_field='entity_content_type',
+        fk_field='entity_object_id',
     )
 
     code = models.CharField(
@@ -48,6 +64,22 @@ class AbstractBranch(Model):
         department = self.department
         name = self.name
         return f'{name}, {department}'
+
+    @property
+    def department(self):
+        """
+        Return the department of the branch
+        :return: the department object
+        """
+
+        if self.entity_content_type.name == 'centre':
+            Class = swapper.load_model('shell', 'Centre')
+        else:
+            Class = swapper.load_model('shell', 'Department')
+
+        department = Class.objects.get(id=self.entity_object_id)
+
+        return department
 
 
 class Branch(AbstractBranch):
