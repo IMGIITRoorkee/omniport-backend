74fe7e12e81bad3d943468f0982b6cab370cf743
pradumangoyal <goyal.praduman611@gmail.com>
2019-12-19 16:51:42 +0530

    Add illustrations to display on login screen with a view to get the count of available illustrations

---
+++ omniport/core/session_auth/views/illustration_roulette.py
@@ -0,0 +1,25 @@
+
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+
+
+class IllustrationRoulette(GenericAPIView):
+    """
+    Return the number of available illustrations to choose from
+    """
+
+    def get(self, request, *args, **kwargs):
+        """
+        View to serve GET requests
+        :param request: the request that is to be responded to
+        :param args: arguments
+        :param kwargs: keyword arguments
+        :return: the response for request
+        """
+
+        response = {
+            'count': 4
+        }
+
+        return Response(response, status=HTTP_200_OK)
