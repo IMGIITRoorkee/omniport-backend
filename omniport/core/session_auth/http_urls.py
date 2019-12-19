59d6ef61f0e72cdef6b7474183782f5da7fa2e2d
pradumangoyal <goyal.praduman611@gmail.com>
2019-12-19 16:51:42 +0530

    Add illustrations to display on login screen with a view to get the count of available illustrations

--- omniport/core/session_auth/http_urls.py
+++ omniport/core/session_auth/http_urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from session_auth.views.login import Login
 from session_auth.views.logout import Logout
+from session_auth.views.illustration_roulette import IllustrationRoulette
 
 app_name = 'session_auth'
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('logout/', Logout.as_view(), name='logout'),
+    path('illustration_roulette/', IllustrationRoulette.as_view(),
+         name='illustration_roulette'),
 ]
