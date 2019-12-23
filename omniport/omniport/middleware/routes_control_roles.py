e7866428d30f5ac7a7d65467d2eb0452a7f5375a
Ayush Jain <ayushjain20n@gmail.com>
2019-12-24 00:15:21 +0530

    Resolve linting errors and bugs

--- omniport/omniport/middleware/routes_control_roles.py
+++ omniport/omniport/middleware/routes_control_roles.py
@@ -4,13 +4,16 @@ from django.conf import settings
 from django.http import Http404
 
 from base_auth.managers.get_user import get_user
+from base_auth.models import User
 from discovery.available import from_acceptable_person
+
 
 class RoutesControlRoles:
     """
     Handles the routing of apps for the guest role and restricts
     the guest user to the allowed urls of an app only
     """
+
     def __init__(self, get_response):
         """
         __init__function exactly as the Django documentation says for
@@ -19,40 +22,45 @@ class RoutesControlRoles:
         self.get_response = get_response
 
     def __call__(self, request):
+        """
+        Perform actual processing on the request before it goes to the view
+        and on response returned by the view
+        :param request: the request being processed
+        :return: the processed response
+        """
 
         source_user = request.user
-        
         try:
             guest_user = get_user(settings.GUEST_USERNAME)
         except User.DoesNotExist:
             guest_user = None
 
-        if source_user == guest_user:
-            """
-            Perform actual processing on the request before it goes to the view
-            and on response returned by the view
-            :param request: the request being processed
-            :return: the processed response
-            """
-            if request.method == 'GET':
-                DISCOVERY = settings.DISCOVERY
+        if source_user != guest_user:
+            response = self.get_response(request)
+            return response
 
-                all_apps = DISCOVERY.apps
+        if request.method != 'GET':
+            raise Http404
 
-                for app, app_configuration in all_apps:
-                    base_url = app_configuration.base_urls.http.strip('/')
-                    if (app_configuration.guest_allowed or
-                    from_acceptable_person(request.roles, 
-                    app_configuration.acceptables.roles)):
-                        excluded_paths = app_configuration.excluded_paths
-                        for excluded_path in excluded_paths:
-                            if re.match(f'^/{base_url}/{excluded_path}/', request.path):
-                                raise Http404
-                    else:
-                        if re.match(f'^/{base_url}/', request.path):
-                            raise Http404
+        DISCOVERY = settings.DISCOVERY
+        all_apps = DISCOVERY.apps
+
+        for app, app_configuration in all_apps:
+            base_url = app_configuration.base_urls.http.strip('/')
+            if (
+                app_configuration.guest_allowed
+                or from_acceptable_person(
+                        request.roles,
+                        app_configuration.acceptables.roles
+                    )
+            ):
+                excluded_paths = app_configuration.excluded_paths
+                for excluded_path in excluded_paths:
+                    if re.match(f'^/{base_url}/{excluded_path}/', request.path):
+                        raise Http404
             else:
-                raise Http404
-        response = self.get_response(request)
+                if re.match(f'^/{base_url}/', request.path):
+                    raise Http404
 
+        response = self.get_response(request)
         return response
