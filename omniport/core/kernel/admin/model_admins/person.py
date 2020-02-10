e58843550a73a1240e3c7fcfb6cfb0d0788e1f6a
rish9101 <rranjan@cs.iitr.ac.in>
2020-02-10 12:03:40 +0530

    Add Generic Relation and reverse relation fields in person entry in omnipotence

--- omniport/core/kernel/admin/model_admins/person.py
+++ omniport/core/kernel/admin/model_admins/person.py
@@ -1,10 +1,90 @@
 import swapper
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
 from formula_one.admin.model_admins.base import ModelAdmin
+from formula_one.models import (
+    ContactInformation,
+    LocationInformation,
+    SocialInformation
+)
 from omniport.admin.site import omnipotence
 
 Person = swapper.load_model('kernel', 'Person')
+BiologicalInformation = swapper.load_model('kernel', 'BiologicalInformation')
+FinancialInformation = swapper.load_model('kernel', 'FinancialInformation')
+PoliticalInformation = swapper.load_model('kernel', 'PoliticalInformation')
+ResidentialInformation = swapper.load_model('kernel', 'ResidentialInformation')
+
+
+class BaseGenericRelationsInline(GenericTabularInline):
+    """
+    This is the base class inherited by all classes implementing
+    inline generic relation in admin page
+    """
+
+    ct_field = 'entity_content_type'
+    ct_fk_field = 'entity_object_id'
+    fk_name = 'entity'
+    max_num = 1
+
+
+class ContactInformationInline(BaseGenericRelationsInline):
+    """
+    This class implements Contact Information Inline for Person in omnipotence
+    """
+
+    model = ContactInformation
+
+
+class LocationInformationInline(BaseGenericRelationsInline):
+    """
+    This class implements Location Information Inline for Person in omnipotence
+    """
+
+    model = LocationInformation
+
+
+class SocialInformationInline(BaseGenericRelationsInline):
+    """
+    This class implements Social Information Inline for Person in omnipotence
+    """
+
+    model = SocialInformation
+
+
+class BiologicalInformationInline(admin.TabularInline):
+    """
+    This class implements Biological Information Inline for Person in
+    omnipotence
+    """
+
+    model = BiologicalInformation
+
+
+class FinancialInformationInline(admin.TabularInline):
+    """
+    This class implements Financial Information Inline for Person in omnipotence
+    """
+
+    model = FinancialInformation
+
+
+class PoliticalInformationInline(admin.TabularInline):
+    """
+    This class implements Political Information Inline for Person in omnipotence
+    """
+
+    model = PoliticalInformation
+
+
+class ResidentialInformationInline(admin.TabularInline):
+    """
+    This class implements Residential Information Inline for Person in
+    omnipotence
+    """
+
+    model = ResidentialInformation
 
 
 @admin.register(Person, site=omnipotence)
@@ -17,3 +97,13 @@ class PersonAdmin(ModelAdmin):
         'full_name',
         'short_name',
     ]
+    inlines = [
+        ContactInformationInline,
+        LocationInformationInline,
+        SocialInformationInline,
+        BiologicalInformationInline,
+        FinancialInformationInline,
+        PoliticalInformationInline,
+        ResidentialInformationInline,
+    ]
+
