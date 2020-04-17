601130d561a2eee8bb2f9f44ec2c1cb083f900da
Ajayneethikannan <ajaykarate001@gmail.com>
2020-04-17 11:51:04 +0530

    Set django to get HTTPS information from X-Forwarded-Proto header

--- omniport/omniport/settings/base/__init__.py
+++ omniport/omniport/settings/base/__init__.py
@@ -5,5 +5,6 @@ from omniport.settings.base.directories import *
 from omniport.settings.base.discovery import *
 from omniport.settings.base.files import *
 from omniport.settings.base.middleware import *
+from omniport.settings.base.security import *
 from omniport.settings.base.shell import *
 from omniport.settings.base.templates import *
---
+++ omniport/omniport/settings/base/security.py
@@ -0,0 +1,5 @@
+"""
+This file defines the security options for Omniport
+"""
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
