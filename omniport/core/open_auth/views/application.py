a01682ed6f4bcf203596cb626ebf395d9523af6f
Kunal <khandelwal.kunal4@gmail.com>
2020-09-28 19:33:34 +0530

    Reformat code in application.py

--- omniport/core/open_auth/views/application.py
+++ omniport/core/open_auth/views/application.py
@@ -1,14 +1,15 @@
 from rest_framework import viewsets, mixins, permissions
 from rest_framework.decorators import action
-
 from django.http import HttpResponse
-from open_auth.models import Application
-from open_auth.serializers.application import ApplicationAuthoriseSerializer
 from oauth2_provider.exceptions import OAuthToolkitError
 from oauth2_provider.models import get_application_model,get_access_token_model
 from oauth2_provider.scopes import get_scopes_backend
 from oauth2_provider.settings import oauth2_settings
 from oauth2_provider.views import AuthorizationView
+
+from open_auth.models import Application
+from open_auth.serializers.application import ApplicationAuthoriseSerializer
+
 
 class ApplicationViewSet(
     mixins.RetrieveModelMixin,
@@ -24,23 +25,20 @@ class ApplicationViewSet(
     lookup_field = 'client_id'
     pagination_class = None
 
-    """
-    Action view for getting the availabilty of token 
-    for a particular view in order to skip authorization
-    after first consent
-    """
     @action(methods=['get'], detail=False, url_name="get_token", url_path="get_token")
     def get_token(self, request):
-        
+        """
+        Action view for getting the availabilty of token 
+        for a particular view in order to skip authorization
+        after first consent
+        """    
+
         token=get_access_token_model().objects.filter(
             user=request.user,
             application=request.GET.get('id')   
         ).exists()
 
-        
         return HttpResponse(token)
-
-
 
 
 class OmniportAuthorizationView(AuthorizationView):
@@ -50,13 +48,13 @@ class OmniportAuthorizationView(AuthorizationView):
     to skip authorization even after the access
     token has expired.
     """
+    
     def get(self, request, *args, **kwargs):
-        
         """
         The code is similar to https://github.com/jazzband/django-oauth-toolkit/blob/master/oauth2_provider/views/base.py
         Significant Changes marked with *** in the code
-         
         """
+
         try:
             require_approval = request.GET.get(
                 "approval_prompt",
@@ -70,9 +68,10 @@ class OmniportAuthorizationView(AuthorizationView):
             all_scopes = get_scopes_backend().get_all_scopes()
             kwargs["scopes_descriptions"] = [all_scopes[scope] for scope in scopes]
             kwargs['scopes'] = scopes
-
             # at this point we know an Application instance with such client_id exists in the database
+
             application = get_application_model().objects.get(client_id=credentials['client_id'])
+
             kwargs['application'] = application
             kwargs['client_id'] = credentials['client_id']
             kwargs['redirect_uri'] = credentials['redirect_uri']
@@ -84,13 +83,11 @@ class OmniportAuthorizationView(AuthorizationView):
             form = self.get_form(self.get_form_class())
             kwargs['form'] = form
             
-            """
-             If skip_authorization field is True, skip the authorization screen even
-             if this is the first use of the application and there was no previous authorization.
-             This is useful for in-house applications-> assume an in-house applications
-             are already approved.
-            """
             
+            #If skip_authorization field is True, skip the authorization screen even
+            #if this is the first use of the application and there was no previous authorization.
+            #This is useful for in-house applications-> assume an in-house applications
+            #re already approved.
             if application.skip_authorization:
                 uri, headers, body, status = self.create_authorization_response(
                     request=self.request, scopes=" ".join(scopes),
@@ -98,8 +95,8 @@ class OmniportAuthorizationView(AuthorizationView):
                 return self.redirect(uri, application)
 
             #check for require_approval
-
             assert require_approval == 'auto_even_if_expired'
+            
             tokens = (
                     get_access_token_model()
                     .objects.filter(
