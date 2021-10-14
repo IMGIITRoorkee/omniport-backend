4d0e33a74cd80695bfa99b72cab0c196040eb0b3
Praduman Goyal <goyal.praduman611@gmail.com>
2021-10-14 18:07:29 +0530

    Update omniport/omniport/settings/base/models.py
    Co-authored-by: Dhruv Bhanushali <dhruv_b@live.com>

--- omniport/omniport/settings/base/models.py
+++ omniport/omniport/settings/base/models.py
@@ -2,4 +2,4 @@
 This settings file exposes the configuration settings for the models and its fields.
 """
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField' 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
